a37b923499a725afaa3b5ba8425a54d77014e945
Eduard <v-epm-eduardgimad@racetrac.com>
2024-11-07 10:25:09 +0300

    delete extra attribute

--- src/test/java/Steps/SomeSteps.java
+++ src/test/java/Steps/SomeSteps.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.When;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SomeSteps {
-    
+
     @When("some step A")
     public void someStepA() {
         System.out.println("some step 1 text");
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,7 +4,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("Steps")
 @SelectClasspathResource("features")
 public class TestRunner {
 }
